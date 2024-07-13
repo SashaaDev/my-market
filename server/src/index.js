dc514d664be5c2301994af927af4b2c9979cb33c
SashaaaaDev <sashahutoranskij15@gmail.com>
2024-07-13 13:18:20 +0300

    added some logs error

--- server/src/index.js
+++ server/src/index.js
@@ -4,11 +4,22 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const errorHandler = require('./middlewares/ErrorHandlingMiddleware');
-const { dbConfig } = require('./config/dbConfig');
+const {dbConfig} = require('./config/dbConfig');
 const router = require('./routes/router');
 const cookieParser = require('cookie-parser');
 
 const port = process.env.PORT || 5000;
+
+if (!process.env.MONGO_URI) {
+  console.error('MONGO_URI is not defined');
+  process.exit(1);
+}
+
+if (!process.env.DB_NAME) {
+  console.error('DB_NAME is not defined');
+  process.exit(1);
+}
+
 app.use(cors({
   origin: 'http://localhost:3000',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
@@ -16,15 +27,13 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(cookieParser());
-mongoose
-    .connect(dbConfig.uri, dbConfig.options)
-    .then(() => {
-      console.log('MongoDB connected');
-    })
+mongoose.connect(dbConfig.uri, dbConfig.options)
+    .then(() => console.log('Successfully connected to MongoDB'))
     .catch((err) => {
       console.error('Error connecting to MongoDB:', err);
+      console.error('URI:', dbConfig.uri);
+      console.error('Options:', dbConfig.options);
     });
-
 
 
 app.get('/', (req, res) => {
